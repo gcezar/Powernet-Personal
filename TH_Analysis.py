82c35211f45ff57881778e44f29587d283301938
gcezar <gcezar@stanford.edu>
2020-05-28 05:03:21 -0700

    updated code and generated graphs

--- TH_Analysis.py
+++ TH_Analysis.py
@@ -44,7 +44,7 @@ class th_analysis():
                                         i.between_time(day_time['night'][0], day_time['night'][1]).std()]]
         return output
 
-    def daily_calculations1(self, df, mode):
+    def daily_th_calc(self, df, mode):
         df = df.resample('H').mean()
         if mode == 'Temp':
             df = (df-32)*5/9    # Convert to Celsius
@@ -54,28 +54,23 @@ class th_analysis():
             output[i.index.day[-1]] = i
         return output
 
-    def daily_calculations2(self, df_t, df_h):
+    def daily_thi_calc(self, df_t, df_h):
+        # Temperature
         df_t = df_t.resample('H').mean()
-        df_t_c = (df_t['Value'] - 32) * 5 / 9  # Convert to Celsius
+        df_t = ((df_t['Value'] - 32) * 5 / 9).to_frame()  # Convert to Celsius
+        # Humidity
         df_h = df_h.resample('H').mean()
-        df_thi = df_t_c - (0.55-0.0055*df_h['Value'])*(df_t_c-58.8)
-        df_thi = df_thi.resample('H').mean()
-        df_t_list = [group[1] for group in df_t.groupby(df_t.index.day)]  # split month into days
-        output_t = {}
-        for t in df_t_list:
-            output_t[t.index.day[-1]] = t
-
-        df_h_list = [group[1] for group in df_h.groupby(df_h.index.day)]  # split month into days
-        output_h = {}
-        for h in df_h_list:
-            output_h[h.index.day[-1]] = h
+        df_h = df_h['Value'].to_frame()
+        # THI
+        df_thi = (1.8*df_t+32) - ((0.55 - 0.0055 * df_h) * (1.8*df_t - 26.8))
+        # df_thi = ser_thi.to_frame()
 
         df_thi_list = [group[1] for group in df_thi.groupby(df_thi.index.day)]  # split month into days
         output_thi = {}
         for th in df_thi_list:
             output_thi[th.index.day[-1]] = th
 
-        return [output_t, output_h, output_thi]
+        return output_thi
 
 ################### MAIN ###################
 
@@ -89,44 +84,51 @@ thi_data = []
 # Analysis
 var = 'Temp'
 # var = 'Hum'
+# var = 'THI'
 # month = 'june'
+# Month = 'June'
 # month = 'july'
+# Month = 'July'
 month = 'august'
+Month = 'August'
+# month = 'september'
+# Month = 'September'
+# month = 'october'
+# Month = 'October'
 
-# INCLUDE THI CALCULATION
-# Equation:
-# THI = T - (0.55-0.0055*H)*(T-58.8)
 for i in sensors:
     obj = th_analysis('lora/'+month+'/grafana_data_export ('+str(i)+').csv')
     data = obj.get_th_dataframe()
     df_t = data[0]
     df_h = data[1]
-    # df_t.head()
-    # print(df_t.index.is_unique)
-    # df_t = (df_t - 32) * 5 / 9  # Convert to Celsius
-    # df_thi = df_t - (0.55 - 0.0055 * df_h) * (df_t - 58.8)
-    temp_data.append(obj.daily_calculations1(df_t,'Temp'))
-    hum_data.append(obj.daily_calculations1(df_h, 'Hum'))
-    thi_data.append(obj.daily_calculations2(df_t, df_h))
 
-sensors_data = {'Temp': temp_data, 'Hum': hum_data}    # temp_data -> sensor_id -> days -> values (m,a,e,n)
-sensors_data2 = {'Temp': thi_data[0], 'Hum': thi_data[1], 'THI': thi_data[2]}
+    temp_data.append(obj.daily_th_calc(df_t,'Temp'))
+    hum_data.append(obj.daily_th_calc(df_h, 'Hum'))
+    thi_data.append(obj.daily_thi_calc(df_t, df_h))
+
+# sensors_data = {'Temp': temp_data, 'Hum': hum_data}    # temp_data -> sensor_id -> days -> values (m,a,e,n)
+sensors_data = {'Temp': temp_data, 'Hum': hum_data, 'THI': thi_data}
 sensor_list = []
 
 for i in range(0,n_sensors):
     sensor_list.append('S'+str(i))
+sensor_list[-1] = 'Sout'
 
 end_day = max(list(sensors_data[var][0]))
 day = min(list(sensors_data[var][0]))
 # day = day + 1
-end_day = day + 1
+# end_day = day + 1
 
+# day = 15
+index = 0
+df_avg = pd.DataFrame(columns=['0'])
+df_std = pd.DataFrame(columns=['0'])
 while day < end_day + 1:
+# while day < 18:
 # day = 4     # day
     daily_data = []
     for s in sensors_data[var]:
         daily_data.append(s[day])
-
 
     time_index = daily_data[0].reset_index()
     time_index['Time'] = pd.to_datetime(time_index['Time'])
@@ -143,12 +145,37 @@ while day < end_day + 1:
 
     if var == 'Hum':
         sns.heatmap(time_index, cmap="RdBu_r",vmin=15, vmax=80, annot=True, fmt=".0f")
-    else:
+    elif var == 'Temp':
         sns.heatmap(time_index, cmap="RdBu_r", vmin=13, vmax=41, annot=True, fmt=".0f")
+    else:
+        sns.heatmap(time_index, cmap="RdBu_r", vmin=55, vmax=85, annot=True, fmt=".0f")
     plt.title(var+': '+date)
     plt.xlabel('Sensor')
     plt.ylabel('Hour of the day')
-    # plt.savefig('lora/'+month+'/plots/'+var+'/'+var+'_'+date+'.png',bbox_inches='tight')
+    plt.savefig('lora/'+month+'/plots/'+var+'/'+var+'_'+date+'.png',bbox_inches='tight')
     plt.show()
     day = day + 1
+    avg = time_index.mean(axis=1).tolist()
+    if len(avg) >= 24:
+        delta = len(avg)-24
+        if delta > 0:
+            avg = avg[:24]
+        df_avg[str(index)] = avg  # hourly avg for each day
 
+    std = time_index.std(axis=1).tolist()
+    if len(std) >= 24:
+        delta = len(std) - 24
+        if delta > 0:
+            std = std[:24]
+        df_std[str(index)] = std  # hourly std for each day
+
+    index = index + 1
+
+avg_thi = df_avg.mean(axis=1)
+std_thi = df_std.mean(axis=1)
+plt.errorbar(avg_thi.index, avg_thi, yerr=std_thi, marker="o")
+plt.title('Average daily THI in ' + Month)
+plt.xlabel('Hour of the day')
+plt.ylabel('THI')
+# plt.savefig('lora/monthly_thi/'+Month+'.png',bbox_inches='tight')
+plt.show()
